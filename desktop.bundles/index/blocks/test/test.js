bda54936cfd9acd6bc2bea2793f730292977ae4b
zakaratcha <zakaratcha@ya.ru>
2018-01-28 17:25:55 +0300

    test cases

--- desktop.bundles/index/blocks/test/test.js
+++ desktop.bundles/index/blocks/test/test.js
@@ -1,11 +1,65 @@
-modules.define('test', ['button', 'test__el', 'i-bem-dom'], function (provide, Button, testEl, bemDom) {
+modules.define('test', ['button', 'test__el', 'i-bem-dom'], function (provide, Button, TestEl, bemDom) {
     provide(bemDom.declBlock(this.name, {
         onSetMod: {
             js: {
                 inited: function () {
-                    console.log(testEl);
+                    var test = this;
+                    var testQueries = {
+
+                        '.test__el': [
+                            TestEl,
+                            {elem: TestEl},
+                            {elem: 'el'}
+                        ],
+
+                        '.test__el_modifyed': [
+                            {elem: TestEl, mods: {modifyed: true}}
+                        ],
+
+                        '[class*="test__el_modifyed_"]': [ // TODO: булевы
+                            {elem: 'el', mods: {modifyed: '*'}}
+                        ],
+
+                        '.test__el:not(.test__el_modifyed)': [
+                            {elem: TestEl, mods: {modifyed: false}},
+                            {elem: 'el', mods: {modifyed: false}}
+                        ],
+
+                        '.button': [
+                            Button,
+                            {block: Button}
+                        ],
+
+                        '.test__button.button': [
+                            {elem: 'button', mix: Button},
+                            {elem: 'button', mix: {block: Button}}
+                        ],
+
+                        '.button.test__button': [
+                            {block: Button, mix: {elem: 'button'}}
+                        ]
+                    };
+
+                    Object.keys(testQueries).forEach(function (key) {
+                        var ok = true;
+                        testQueries[key].forEach(function (query) {
+                            var result = this._buildSelector(query, test);
+                            if (result !== key) {
+                                console.log('Building selector error');
+                                console.log('must be: ', key);
+                                console.log('builded: ', result);
+                                ok = false;
+                            }
+                        });
+                        if (ok) {
+                            console.log('OK: ', key);
+                        }
+                    });
                 }
             }
+        },
+        _buildSelector: function (entity, ctx) {
+            return '';
         }
     }));
 });
--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -3,12 +3,12 @@ module.exports = {
     title: 'Title of the page',
     favicon: '/favicon.ico',
     head: [
-        { elem: 'meta', attrs: { name: 'description', content: '' } },
-        { elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
-        { elem: 'css', url: 'index.min.css' }
+        {elem: 'meta', attrs: {name: 'description', content: ''}},
+        {elem: 'meta', attrs: {name: 'viewport', content: 'width=device-width, initial-scale=1'}},
+        {elem: 'css', url: 'index.min.css'}
     ],
-    scripts: [{ elem: 'js', url: 'index.min.js' }],
-    mods: { theme: 'islands' },
+    scripts: [{elem: 'js', url: 'index.min.js'}],
+    mods: {theme: 'islands'},
     content: [
         {
             block: 'content',
